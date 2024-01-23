86f97ae2bc665f28783ff886f73801328ccb4359
Iryna Chervinska <iryna_chervinska@epam.com>
2024-01-23 23:27:07 +0200

    added integration test

---
+++ song-service/src/test/java/com/example/songservice/service/SongServiceImplTest.java
@@ -0,0 +1,41 @@
+package com.example.songservice.service;
+
+import com.example.songservice.model.Song;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+public class SongServiceImplTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private SongService songService;
+
+    @Test
+    public void getSongById() throws Exception {
+        Song song = new Song(1L, "Song1", "artist", null, null, null, 1L);
+        when(songService.getSongById(1L)).thenReturn(song);
+
+        mockMvc.perform(get("/songs/1"))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        verify(songService, times(1)).getSongById(1L);
+    }
+}
